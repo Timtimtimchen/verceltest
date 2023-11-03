1e9372ab6cd4c48e8c747364239726378ba038fa
Timtimtimchen <tim301065@gmail.com>
2023-11-03 10:35:19 +0800

    3

--- index.py
+++ index.py
@@ -4,10 +4,7 @@ app = Flask(__name__,static_url_path="/resume")
 
 @app.route("/")
 def index():
-    # 请替换 '你的PDF文件.pdf' 为你要显示的实际PDF文件的路径和文件名
-    pdf_path = 'static\\履歷.pdf'
-    # 使用send_file函数发送PDF文件
-    return send_file(pdf_path, as_attachment=False)
+    return render_template("index.html")
 
 app.run(debug = True)
 
